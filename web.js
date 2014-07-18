b3d8f0b25f58079f9f828aeeb6435a657aaf6277
Alexandra Swart <answart@sbcglobal.net>
2014-07-18 14:20:58 -0700

    Add wufoo-node wrapper for Wufoo API

--- web.js
+++ web.js
@@ -13,3 +13,25 @@
 // app.listen(port, function() {
 //   console.log("Listening on " + port);
 // });
+
+   var Wufoo = require("wufoo");
+   var wufoo = new Wufoo("answart", "2BJ3-IKC6-LSJU-227P");
+
+   wufoo.getForms(function(err, forms) {
+      // do something with your forms here.
+      console.log(forms);
+   });
+
+   // get a specific form given the id.
+   wufoo.getForm("idofForm", function(err, form){
+      // do something with your form here.
+      
+   });
+
+   wufoo.getFormEntries("idofForm", function(err, entries) {
+      // do something with your entries here.
+      console.log(entries)
+   });
+
+
+wufoo.getForm("q2260j51chrkf6");
