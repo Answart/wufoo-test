dc369a1acdbfa7b5bd664393a73ebbd67e51da5a
Alexandra Swart <answart@sbcglobal.net>
2014-07-21 15:59:04 -0700

    trying different export styles

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser');
+app.use( bodyParser.json() );
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -163,6 +164,11 @@ app.get('/api/v1/getWidgets', getWidgets);
 app.post('/api/v1/notifications', function(request, response) {
     response.setHeader('Content-Type', 'application/json');
     response.end(JSON.stringify(request.body));
+    console.log("+++++++++++++++++")
+    console.log(JSON.stringify(request.body));
+    console.log("- - - - - - - - - - -")
+    console.log('req.body.name', request.body['name']);
+    console.log("====================")
 });
 
 var server = app.listen(process.env.PORT || 3000, function() {
